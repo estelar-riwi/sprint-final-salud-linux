6d9cca92a054c1036b9771ca51ce01f3a907cd70
Jhonatan M <jhonatanmorales99@gmail.com>
2025-10-03 16:11:39 -0500

    first Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace sprint_final_salud_linux.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
