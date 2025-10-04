a7c6cf27ed122525d64364f4905a7b058313cb69
alejo11102001 <diego.zuluagaye@amigo.edu.co>
2025-10-03 20:20:24 -0500

    Feat: add crud users and styles

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using sprint_final_salud_linux.Data;
+using sprint_final_salud_linux.Models;
 namespace sprint_final_salud_linux.Controllers;
 
 public class AdminController : Controller
@@ -13,27 +14,91 @@ public class AdminController : Controller
 
     public IActionResult Index()
     {
-        return View();
+        var users = _context.Users;
+        return View(users);
     }
 
-    public IActionResult Register()
+    public IActionResult Create()
     {
         return View();
     }
 
+    [HttpPost]
+    public IActionResult Store([Bind("Name,Identification,Phone")] User user)
+    {
+        if (ModelState.IsValid)
+        {
+            var existsIdentification = _context.Users.Any(u => u.Identification == user.Identification);
+            if (existsIdentification)
+            {
+                ModelState.AddModelError("Identification", "Esta identificación ya está registrada.");
+                return View("Create", user);
+            }
+            
+            var existisPhone = _context.Users.Any(u => u.Phone == user.Phone);
+            if (existisPhone)
+            {
+                ModelState.AddModelError("phone", "Este telefono ya está registrado.");
+                return View("Create", user);           
+            }
+            
+            _context.Add(user);
+            _context.SaveChanges();
+            TempData["message"] = "Usuario creado";
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View("Create", user);
+    }
+    
     public IActionResult Carnet()
     {
         return View();
     }
 
-    public IActionResult Delete()
+    public IActionResult Delete(int Id)
     {
-        return View();
+        var user = _context.Users.Find(Id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+        
+        _context.Users.Remove(user);
+        _context.SaveChanges();
+        TempData["message"] = "Cliente eliminado";
+        return RedirectToAction(nameof(Index));
     }
 
-    public IActionResult Edit()
+    public IActionResult Edit(int Id)
     {
-        return View();
+        var user = _context.Users.Find(Id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+        return View(user);
+    }
+
+    public IActionResult Update(int Id, User updateUser)
+    {
+        var user = _context.Users.Find(Id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        if (ModelState.IsValid)
+        {
+            user.Name = updateUser.Name;
+            user.Identification = updateUser.Identification;
+            user.Phone = updateUser.Phone;
+            _context.SaveChanges();
+            TempData["message"] = "Usuario actualizado";
+            return RedirectToAction(nameof(Index));
+        }
+
+        return View(updateUser);
     }
 
     public IActionResult NextTurn()
