1a8f632d518e1d0531cf4704a7c602ac4721da22
Vanessa55-rgb <vanessagomezlopez55@gmail.com>
2025-10-08 19:25:55 -0500

    styles, turn,full screen functionality

--- Controllers/TurnController.cs
+++ Controllers/TurnController.cs
@@ -3,57 +3,61 @@ using Microsoft.AspNetCore.SignalR;
 using sprint_final_salud_linux.Data;
 using sprint_final_salud_linux.Models;
 using sprint_final_salud_linux.Signal;
-namespace sprint_final_salud_linux.Controllers;
 
-public class TurnController :Controller
+namespace sprint_final_salud_linux.Controllers;
+public class TurnController : Controller
 {
     private readonly MySqlContext _context;
     private readonly IHubContext<SignalR> _hubContext;
 
-    public TurnController(MySqlContext context, IHubContext<SignalR> hubContext)
-    {
-        _context = context;
-        _hubContext = hubContext;
+    public TurnController(MySqlContext context,IHubContext<SignalR> hubContext){
+        _context=context;
+        _hubContext=hubContext;
     }
 
-    public IActionResult RequestTurn()
-    {
-        return View();
-    }
+    public IActionResult RequestTurn(){ return View(); }
 
     [HttpPost]
-    public async Task<IActionResult> RequestTurnPost()
-    {
-        var turn = _context.Turns.FirstOrDefault(t => t.Id == 1);
-        if (turn == null)
-        {
-            turn = new Turn { Id = 1, CurrentTurn = 0, NextTurn = 1, TurnRequest = 1 };
+    public async Task<IActionResult> RequestTurnPost(){
+        var turn = _context.Turns.FirstOrDefault(t=>t.Id==1);
+        if(turn==null){
+            turn = new Turn{Id=1,CurrentTurn=0,NextTurn=1,TurnRequest=1};
             _context.Turns.Add(turn);
             await _context.SaveChangesAsync();
         }
+
         int assigned = turn.TurnRequest;
-        
-        var request = new TurnRequest
-        {
-            Number = assigned,
-        };
+
+        var request = new TurnRequest{Number=assigned};
         _context.TurnRequests.Add(request);
-        
-        turn.TurnRequest = (turn.TurnRequest % 100) + 1;
 
+        turn.TurnRequest = (turn.TurnRequest % 100) +1;
         await _context.SaveChangesAsync();
-
-        TempData["assignedTurn"] = assigned;
-
-        // Avisar a todos que hay un nuevo turno pedido
+        
         await _hubContext.Clients.All.SendAsync("NuevoTurnoSolicitado", assigned);
+        
+        return Json(new { turno = assigned });
+    }
+    public IActionResult TurnConfirmation(){ return View(); }
 
-        return RedirectToAction("TurnConfirmation");
+    public IActionResult TurnView(){
+        var turn = _context.Turns.FirstOrDefault(t=>t.Id==1);
+        if(turn==null){ turn = new Turn{Id=1,CurrentTurn=0,NextTurn=1,TurnRequest=1}; }
+        return View(turn);
     }
 
-    public IActionResult TurnConfirmation()
-    {
-        ViewBag.Turno = TempData["assignedTurn"];
-        return View();
+    [HttpPost]
+    public async Task<IActionResult> ResetTurnsConfirmed(){
+        var turn = _context.Turns.FirstOrDefault(t=>t.Id==1);
+        if(turn!=null){
+            turn.CurrentTurn=0;
+            turn.NextTurn=1;
+            turn.TurnRequest=1;
+            await _context.SaveChangesAsync();
+        }
+        await _hubContext.Clients.All.SendAsync("ActualizarTurnos", 0, 1, "-");
+        TempData["message"]="Turnos reiniciados correctamente.";
+        return RedirectToAction("ResetTurns");
     }
+    public IActionResult ResetTurns(){ return View(); }
 }
